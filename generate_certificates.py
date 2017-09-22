b4ffe56b9091ca887df9926b9ea2a6023c1d426f
Dmitry Ledentsov <dmlled@yahoo.com>
2017-09-22 21:40:05 +0200

    more certificates

--- generate_certificates.py
+++ generate_certificates.py
@@ -7,8 +7,11 @@ import sys
 import os
 
 if __name__ == '__main__':
-    peers = ['pony-server']
+    peers = [
+        ['pony-server', 'server'],
+        ['pony-server', 'client'],
+    ]
 
-    for name in peers:
-        public_file, secret_file = zmq.auth.create_certificates(name, "tmp")
+    for names in peers:
+        public_file, secret_file = zmq.auth.create_certificates(names[0], names[1])
         print("Generated: "+str(public_file)+", "+str(secret_file))
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 echo --=== generating the certificates ===--
 ./generate_certificates.py
-cp pony-server/tmp.key server.key
+cp pony-server/server.key server.key
 
 echo --=== building and starting the containers ===--
 docker-compose up --build
