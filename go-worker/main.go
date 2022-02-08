2848677888a3cb0d8c6636496574fb0ab2da57a8
Dmitry Ledentsov <dmlled@yahoo.com>
2022-02-08 18:02:10 +0100

    debugging continued

--- go-worker/main.go
+++ go-worker/main.go
@@ -1,88 +1,97 @@
 package main
 
 import (
-    "fmt"
-    zmq "github.com/pebbe/zmq4"
-    "io/ioutil"
-    "regexp"
-    "time"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"time"
+
+	zmq "github.com/pebbe/zmq4"
 )
 
 func main() {
-    fmt.Println("Starting go worker")
+	fmt.Println("Starting go worker")
 
-    client_public, client_secret, err := readKeysFrom("client.key_secret")
-    panicIfError(err)
+	client_public, client_secret, err := readKeysFrom("client.key_secret")
+	panicIfError(err)
 
-    server_public, _, err := readKeysFrom("server.key")
-    panicIfError(err)
+	server_public, _, err := readKeysFrom("server.key")
+	panicIfError(err)
 
-    push, _ := zmq.NewSocket(zmq.PUSH)
-    push.ClientAuthCurve(server_public, client_public, client_secret)
-    push.Connect("tcp://pony-server:7778")
-    defer push.Close()
+	ponyServer := os.Getenv("PONY_SERVER")
+	if ponyServer == "" {
+		ponyServer = "pony-server"
+	}
 
-    pull, _ := zmq.NewSocket(zmq.PULL)
-    pull.ClientAuthCurve(server_public, client_public, client_secret)
-    pull.Connect("tcp://pony-server:7777")
-    defer pull.Close()
+	push, err := zmq.NewSocket(zmq.PUSH)
+	panicIfError(err)
+	panicIfError(push.ClientAuthCurve(server_public, client_public, client_secret))
+	panicIfError(push.Connect("tcp://" + ponyServer + ":7778"))
+	defer push.Close()
 
-    poller := zmq.NewPoller()
-    poller.Add(pull, zmq.POLLIN)
+	pull, err := zmq.NewSocket(zmq.PULL)
+	panicIfError(err)
+	panicIfError(pull.ClientAuthCurve(server_public, client_public, client_secret))
+	panicIfError(pull.Connect("tcp://" + ponyServer + ":7777"))
+	defer pull.Close()
 
-    for {
-        sockets, _ := poller.Poll(20 * time.Second)
-        if len(sockets) == 0 {
-            break
-        }
+	poller := zmq.NewPoller()
+	poller.Add(pull, zmq.POLLIN)
 
-        message, err := sockets[0].Socket.Recv(0)
-        panicIfError(err)
+	for {
+		sockets, _ := poller.Poll(20 * time.Second)
+		if len(sockets) == 0 {
+			break
+		}
 
-        fmt.Println("Go worker received:", message)
-        _, err = push.Send("Go worker says: "+message, 0)
-        panicIfError(err)
-    }
+		message, err := sockets[0].Socket.Recv(0)
+		panicIfError(err)
 
-    fmt.Println("Exiting the go worker")
+		fmt.Println("Go worker received:", message)
+		_, err = push.Send("Go worker says: "+message, 0)
+		panicIfError(err)
+	}
+
+	fmt.Println("Exiting the go worker")
 }
 
 func panicIfError(err error) {
-    if err != nil {
-        fmt.Println("Go worker ERROR: ", err)
-        panic(err)
-    }
+	if err != nil {
+		fmt.Println("Go worker ERROR: ", err)
+		panic(err)
+	}
 }
 
 func readKeysFrom(filename string) (string, string, error) {
-    secret_regex, err := regexp.Compile("secret-key\\s*\\=\\s*\"(\\S+?)\"")
-    if err != nil {
-        return "", "", err
-    }
+	secret_regex, err := regexp.Compile("secret-key\\s*\\=\\s*\"(\\S+?)\"")
+	if err != nil {
+		return "", "", err
+	}
 
-    public_regex, err := regexp.Compile("public-key\\s*\\=\\s*\"(\\S+?)\"")
-    if err != nil {
-        return "", "", err
-    }
+	public_regex, err := regexp.Compile("public-key\\s*\\=\\s*\"(\\S+?)\"")
+	if err != nil {
+		return "", "", err
+	}
 
-    b, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return "", "", err
-    }
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", "", err
+	}
 
-    var secret = ""
+	var secret = ""
 
-    secret_found := secret_regex.FindSubmatch(b)
-    if secret_found != nil {
-        secret = string(secret_found[1])
-    }
+	secret_found := secret_regex.FindSubmatch(b)
+	if secret_found != nil {
+		secret = string(secret_found[1])
+	}
 
-    var public = ""
+	var public = ""
 
-    public_found := public_regex.FindSubmatch(b)
-    if public_found != nil {
-        public = string(public_found[1])
-    }
+	public_found := public_regex.FindSubmatch(b)
+	if public_found != nil {
+		public = string(public_found[1])
+	}
 
-    return public, secret, nil
+	return public, secret, nil
 }
